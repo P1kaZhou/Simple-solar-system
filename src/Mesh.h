c2d9f59cf70fbea25640d85eadf99d5a40aea65b
Zhou <daniel.zhou.fr@gmail.com>
2022-10-19 19:06:58 +0200

    AWH HELL NEOW

--- src/Mesh.h
+++ src/Mesh.h
@@ -179,13 +179,16 @@ public:
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
         glEnableVertexAttribArray(1);
 
-        glGenBuffers(1, &m_ibo);
-        glBindBuffer(GL_ARRAY_BUFFER, m_ibo);
 
-
+        // todo: ??????
         // Same for an index buffer object that stores the list of indices of the
         // triangles forming the meshSun
         size_t indexBufferSize = sizeof(unsigned int) * m_triangleIndices.size();
+        glGenBuffers(1, &m_ibo);
+        glBindBuffer(GL_ARRAY_BUFFER, m_ibo);
+        glBufferData(GL_ARRAY_BUFFER, indexBufferSize, m_triangleIndices.data(), GL_DYNAMIC_READ);
+        glVertexAttribPointer(2, 3, GL_INT, GL_FALSE, 3 * sizeof(GLint), 0);
+        glEnableVertexAttribArray(2);
 
 
         glBindVertexArray(0); // deactivate the VAO for now, will be activated again when rendering
